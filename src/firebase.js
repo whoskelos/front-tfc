9354f39fc4adf0e784a46c7989fa86abc569d73a
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2024-09-28 02:31:32 +0200

    edit: gitignore, hide keys

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDaZ41AGgRqfR2ib6NtYoNSfPsImhfuVk",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "gamekeyzone-app.firebaseapp.com",
   projectId: "gamekeyzone-app",
   storageBucket: "gamekeyzone-app.appspot.com",
-  messagingSenderId: "410241484839",
-  appId: "1:410241484839:web:fcde98aef5cb01f1c46602"
+  messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import AuthProvider from "./context/AuthContext.jsx"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PayPalScriptProvider options={{
-      "clientId": "AT70eShqd3DS7L5A883l_6ej6DY-MluYU5IzNdOYsJ8nkEFub7gmY5dRiA5h1966vb6jClZfanskjrr2",
+      "clientId": import.meta.env.VITE_PAYPAL_CLIENTID,
       "currency": "EUR",
     }}>
       <AuthProvider>
